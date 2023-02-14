27c6f60f7b98df1b5b8ec70421a6214497010715
hryashik <tsukikohaya@gmail.com>
2023-02-14 18:39:11 +0300

    init project

---
+++ gateway/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {ConfigService} from "./services/config/config.service";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(new ConfigService().get('GATEWAY_PORT'))
+}
+bootstrap();
---
+++ gateway/src/services/config/config.service.ts
@@ -0,0 +1,16 @@
+export class ConfigService {
+    private readonly envConfig: { [key: string]: any } = null;
+    constructor() {
+        this.envConfig = {}
+        this.envConfig.gatewayPort = process.env.GATEWAY_PORT
+        this.envConfig.userService = {
+            options: {
+                port: process.env.USER_SERVICE_PORT,
+                host: process.env.USER_SERVICE_HOST,
+            },
+        }
+    }
+    public get(key: string): any {
+        return this.envConfig[key]
+    }
+}
